5118c58b6444d3b0c23458ffbae755c9ff91d35d
Yaroslav <yarikdrapey@gmail.com>
2023-06-28 23:31:22 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// function burger(burgerSel, menuSel, breakpoint) {
+//   const burger = document.querySelector(burgerSel),
+//         menu = document.querySelector(menuSel)
+//   burger.addEventListener('click', () => {
+//     if (parseInt(getComputedStyle(document.body).width) <= breakpoint) {
+//       burger.classList.toggle('active')
+//       menu.classList.toggle('active')
+//       document.body.classList.toggle('lock')
+//     }
+//   })
+// }
