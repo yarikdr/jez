3ddb066493a4aace72be8075c18ca8448137836e
Yaroslav <yarikdrapey@gmail.com>
2023-06-29 10:19:26 +0300

    block header and all burger functionality

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-// function burger(burgerSel, menuSel, breakpoint) {
-//   const burger = document.querySelector(burgerSel),
-//         menu = document.querySelector(menuSel)
-//   burger.addEventListener('click', () => {
-//     if (parseInt(getComputedStyle(document.body).width) <= breakpoint) {
-//       burger.classList.toggle('active')
-//       menu.classList.toggle('active')
-//       document.body.classList.toggle('lock')
-//     }
-//   })
-// }
+function burger(burgerSel, menuSel, breakpoint) {
+  const burger = document.querySelector(burgerSel),
+        menu = document.querySelector(menuSel)
+  burger.addEventListener('click', () => {
+    if (parseInt(getComputedStyle(document.body).width) <= breakpoint) {
+      burger.classList.toggle('active')
+      menu.classList.toggle('active')
+      document.body.classList.toggle('lock')
+    }
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  burger('.burger', '.menu', 768)
+})
