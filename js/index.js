8092d4746ceccac4b02f893a7f417105bbc254a0
Yaroslav <yarikdrapey@gmail.com>
2023-06-30 15:51:10 +0300

    the block slider is finished; class wrapper reworked

--- js/index.js
+++ js/index.js
@@ -21,7 +21,66 @@ function watchScroll() {
   })
 }
 
+function slider(swiper, prev, next, slideSel) {
+  new Swiper(swiper, {
+    navigation: {
+      nextEl: next,
+      prevEl: prev,
+    },
+
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    autoHeight: true,
+    centeredSlides: true,
+    breakpoint: {
+      768: {
+        centeredSlides: false,
+      }
+    }
+  })
+
+  function fraction(slideSel) {
+    function getZero(num) {
+      console.log(num)
+      if (num < 10) return `0${num}`
+      return num
+    }
+
+    let slideIndex = 1
+
+    const current = document.querySelector('#currentSlide'),
+          total = document.querySelector('#totalSlides'),
+          numberOfSlides = document.querySelectorAll(slideSel).length,
+          prev = document.querySelector('#prev'),
+          next = document.querySelector('#next')
+
+    total.textContent = getZero(numberOfSlides)
+    current.textContent = getZero(slideIndex)
+
+    function changeSlide(n) {
+      if (n > numberOfSlides) {
+        slideIndex = numberOfSlides
+      } else if (n < 0) {
+        slideIndex = 1
+      }
+
+      slideIndex = n
+    }
+    
+    function plusSlide(n) {
+      changeSlide(slideIndex += n)
+      current.textContent = getZero(slideIndex)
+    }
+
+    prev.addEventListener('click', () => plusSlide(-1))
+    next.addEventListener('click', () => plusSlide(1))
+  }
+
+  fraction(slideSel)
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   burger('.burger', '.menu', 768)
   watchScroll()
+  slider('.swiper', '#prev', '#next', '.swiper-slide')
 })
